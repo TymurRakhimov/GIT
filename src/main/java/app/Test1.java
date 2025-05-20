6dac0f54375c8feed661333b711d32adc5e09eb1
TymurRakhimov <modest11@web.de>
2025-05-20 16:49:16 +0200

    Test task 2 bugs fixed!!!

--- src/main/java/app/Test1.java
+++ src/main/java/app/Test1.java
@@ -3,7 +3,7 @@ package app;
 public class Test1 {
 
     public static void main(String[] args) {
-        System.out.println("This is first testing code,version 2!");
+        System.out.println("This is first testing code,version 3!");
 
         System.out.println("And something else!");
 
--- src/main/java/app/Test2.java
+++ src/main/java/app/Test2.java
@@ -2,6 +2,6 @@ package app;
 
 public class Test2 {
     public static void main(String[] args) {
-        System.out.println("This is second testing code!");
+        System.out.println("This is second testing code,version 2!");
     }
 }
